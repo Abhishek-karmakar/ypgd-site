a5808f87b13e2cccd44bff8ef01b84f9aa102cd8
ajaybhatia <prof.ajaybhatia@gmail.com>
2016-05-14 00:06:40 +0530

    Fixed paths

--- helpers/db_connect.php
+++ helpers/db_connect.php
@@ -14,11 +14,9 @@ class DB_Connect {
  
     // Connecting to database
     public function connect() {
-        require_once 'config.php';
+        require_once 'config/config.php';
         // connecting to mysql
-        $con = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-        // selecting database
-        mysql_select_db(DB_DATABASE);
+        $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
  
         // return database handler
         return $con;
